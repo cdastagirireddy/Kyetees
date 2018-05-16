ee61ba11a4cc6d637975536c128619b1e59d0c76
admin <cdastagirireddy@hotmail.com>
2018-05-16 13:55:40 +0530

    java files

--- app/src/main/java/com/kuncham/kyetees/MainActivity.java
+++ app/src/main/java/com/kuncham/kyetees/MainActivity.java
@@ -1,20 +1,71 @@
 package com.kuncham.kyetees;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
 
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private HomeFragment mHomeFragment;
+    private MensFragment mMensFragment;
+    private WomensFragment mWomensFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //BottemNavigation Framework
+        mMainFrame = (FrameLayout)findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView)findViewById(R.id.main_nav);
+
+        mHomeFragment = new HomeFragment();
+        mMensFragment = new MensFragment();
+        mWomensFragment = new WomensFragment();
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId())
+                {
+                    case R.id.home :
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(mHomeFragment);
+                        return true;
+                    case R.id.mens :
+                        mMainNav.setItemBackgroundResource(R.color.colorAccent);
+                        setFragment(mMensFragment);
+
+                        return true;
+
+                    case R.id.womens:
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimaryDark);
+                        setFragment(mWomensFragment);
+
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
+
+
+        //DrawerLayout
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
 
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
@@ -32,5 +83,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
     }
 }
